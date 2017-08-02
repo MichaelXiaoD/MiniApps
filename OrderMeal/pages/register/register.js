17dfa25830f44b2397c17c09e0fc1844a7837266
MichaelXiaoD <372318565@qq.com>
2017-08-02 22:52:00 +0800

    注册页视图层
    1. 注册页视图层基本完成
    2.菜单页添加了一个NAVIGATOR控制跳转到注册页

--- OrderMeal/pages/register/register.js
+++ OrderMeal/pages/register/register.js
@@ -1,66 +1,88 @@
-// register.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    array: ['493主控室', '493机械组', '493电气组', '三废室'],
+    objectArray: [
+      {
+        id: 0,
+        name: '493主控室'
+      },
+      {
+        id: 1,
+        name: '493机械组'
+      },
+      {
+        id: 2,
+        name: '493电气组西'
+      },
+      {
+        id: 3,
+        name: '三废室'
+      }
+    ],
+    index: 0,
   },
-
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+    
   }
 })
